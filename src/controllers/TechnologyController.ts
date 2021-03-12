eb9e0435c763cd4fb1b2dea2971e58fe1d488b7c
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-03-12 14:08:34 -0300

    Final Project

--- src/controllers/TechnologyController.ts
+++ src/controllers/TechnologyController.ts
@@ -1,4 +1,4 @@
-import {Request,Response} from 'express'
+import {Request,response,Response} from 'express'
 import { getRepository } from 'typeorm';
 import { Technology } from '../models/Technology';
 
@@ -22,6 +22,40 @@ class TechnologyController{
         });
         technologiesRepository.save(technology);
         return res.status(200).json({message: "Tecnologia criada com sucesso"});
+    }
+    async index(req: Request, res: Response){
+        const technologiesRepository = getRepository(Technology);
+        const technologies = await technologiesRepository.find();
+        return res.json(technologies);
+    }
+    async show(req: Request, res: Response){
+        const {id} = req.params;
+        const technologiesRepository = getRepository(Technology);
+        const technology = await technologiesRepository.findOne({id});
+        if(!technology){
+            return res.status(400).json({error: "A tecnologia não existe"});
+        }
+        return res.json(technology);
+    }
+    async update(req: Request, res: Response){
+        const {description,article} = req.body;
+        const {id} = req.params;
+        const technologiesRepository = getRepository(Technology);
+        const technologyUpdate = await technologiesRepository.update({id},{description, article});
+        
+        return res.json({message: "Tecnologia alterada com sucesso"});
+    }
+    async delete(req:Request,res:Response){
+        const {id} = req.params;
+        const technologiesRepository = getRepository(Technology);
+        const technology = technologiesRepository.findOne({id});
+        if(!technology){
+            return res.status(401).json({error: "A tecnologia não existe"});
+        }
+        const technologyDelete = await technologiesRepository.delete({id});
+        technologyDelete;
+        return res.json({message: 'deletado com sucesso'})
+        
     }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,9 @@ import { TechnologyController } from './controllers/TechnologyController';
 const technologyController = new TechnologyController;
 const router = Router();
 
-router.post("/technology", technologyController.create)
+router.post("/technology", technologyController.create);
+router.get("/technology",technologyController.index);
+router.get("/technology/:id",technologyController.show);
+router.put("/technology/:id",technologyController.update);
+router.delete("/technology/:id",technologyController.delete);
 export {router};
